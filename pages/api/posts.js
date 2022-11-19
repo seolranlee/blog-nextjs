ca303ac8dd387a1b09d083b72a9f853b2cf25d5f
Seolran LEE <seolranlee@Seolrans-MacBook-Air.local>
2022-11-19 14:57:35 +0900

    블로그 프로젝트: 03. Pre-rendering - Data Fetching

---
+++ pages/api/posts.js
@@ -0,0 +1,8 @@
+import { getSortedPostsData } from '../../lib/posts'
+
+export default function handler(req, res) {
+  const allPostsData = getSortedPostsData()
+  res.status(200).json({
+    allPostsData,
+  })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,62 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Layout, { siteTitle } from '../components/layout'
+// import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+// SSG
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   }
+// }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3000/api/posts')
+  const json = await response.json()
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
+  // CSR
+  // const [allPostsData, setAllPostsData] = useState([])
+  // useEffect(() => {
+  //   fetch('/api/posts')
+  //     .then((res) => res.json())
+  //     .then((data) => setAllPostsData(data.allPostsData))
+  // }, [])
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          생활의 전문가와 소비자를 연결하는 O2O 플랫폼 <strong>숨고</strong>를
+          운영하는 <strong>브레이브 모바일</strong>의 프론트엔드 엔지니어로
+          재직중입니다. 사용자가 원하는 지점에 도달하기까지의 프로세스 전반을
+          개선하는 일에 관심이 많습니다.
         </p>
+        <h2 className={utilStyles.headingLg}>Posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
