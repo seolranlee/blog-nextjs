bb0454bd65bb5f0732aa82c913fa7a9b5e3e1460
Seolran LEE <seolranlee@Seolrans-MacBook-Air.local>
2023-02-19 13:34:51 +0900

    add sitemap, robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://blog-nextjs-weld-ten.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
